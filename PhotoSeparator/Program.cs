bcc2329e60fed4f6a913f416cd2b0b46edf35ed8
Nick Shepherd <shepherdnick@hotmail.com>
2019-04-21 12:32:25 +0100

    First pass at making it more generic.

--- PhotoSeparator/Program.cs
+++ PhotoSeparator/Program.cs
@@ -9,6 +9,20 @@ namespace PhotoSeparator
     {
         static void Main(string[] args)
         {
+            PhotoSorter photoSorter = new PhotoSorter();
+        }
+    }
+
+    class PhotoSorter
+    {
+        public PhotoSorter()
+        {
+            //SortPhotosIntoFolders();
+            SortExistingPhotosIntoCorrectFolder();
+        }
+
+        private void SortPhotosIntoFolders()
+        {
             int folderNumber = 1;
 
             while (folderNumber < 20)
@@ -21,75 +35,143 @@ namespace PhotoSeparator
                 foreach (var file in files)
                 {
                     //string file = @"D:\\Photos\\data-download-1\\01022008_2265470602_o.jpg";
-                    try
-                    {
-                        var tryToGetDirectories = ImageMetadataReader.ReadMetadata(file);
-                    }
-                    catch(Exception ex)
-                    {
-                        continue;
-                    }
-
-                    var directories = ImageMetadataReader.ReadMetadata(file);
-
-                    foreach (var directory in directories)
-                    {
-                        foreach (var tag in directory.Tags)
-                        {
-                            if (tag.Name.Equals("Date/Time Original"))
-                            {
-                                Console.WriteLine($"[{directory.Name}] {tag.Name} = {tag.Description}");
-
-                                var fileDateTime = DateTime.ParseExact(tag.Description, "yyyy:MM:dd HH:mm:ss", new CultureInfo("en-GB"));
-
-                                //var destinationDirectory = Path.GetDirectoryName(file) + "\\" + fileDateTime.Year + "-" + fileDateTime.Month + "-" + fileDateTime.Day;
-                                var destinationDirectory = @"D:\\Photos\\Sorted\\" + fileDateTime.Year + "-" + fileDateTime.Month + "-" + fileDateTime.Day;
-                                Console.WriteLine(destinationDirectory);
-
-                                if (!System.IO.Directory.Exists(destinationDirectory))
-                                {
-                                    System.IO.Directory.CreateDirectory(destinationDirectory);
-                                }
-
-                                File.Move(file, destinationDirectory + "\\" + Path.GetFileName(file));
-                            }
-
-                            if (tag.Name.Equals("Created"))
-                            {
-                                Console.WriteLine($"[{directory.Name}] {tag.Name} = {tag.Description}");
-
-                                //Fri Apr 01 04:25:42 2011
-
-                                var fileDateTime = DateTime.ParseExact(tag.Description, "ddd MMM dd HH:mm:ss yyyy", new CultureInfo("en-GB"));
-
-                                //var destinationDirectory = Path.GetDirectoryName(file) + "\\" + fileDateTime.Year + "-" + fileDateTime.Month + "-" + fileDateTime.Day;
-                                var destinationDirectory = @"D:\\Photos\\Sorted\\" + fileDateTime.Year + "-" + fileDateTime.Month + "-" + fileDateTime.Day;
-                                Console.WriteLine(destinationDirectory);
-
-                                if (!System.IO.Directory.Exists(destinationDirectory))
-                                {
-                                    System.IO.Directory.CreateDirectory(destinationDirectory);
-                                }
-
-                                if (File.Exists(file))
-                                {
-                                    File.Move(file, destinationDirectory + "\\" + Path.GetFileName(file));
-                                }
-                            }
-                        }
-
-                        if (directory.HasError)
-                        {
-                            foreach (var error in directory.Errors)
-                                Console.WriteLine($"ERROR: {error}");
-                        }
-                    }
+                    SortFileIntoFolder(file, "D:\\Photos\\Sorted\\");
 
                     Console.WriteLine("=======================================================");
                 }
-            }            
+            }
 
             Console.ReadLine();
+        }
+
+        private void SortFileIntoFolder(string file, string destinationPath)
+        {
+            try
+            {
+                var tryToGetDirectories = ImageMetadataReader.ReadMetadata(file);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+
+            var directories = ImageMetadataReader.ReadMetadata(file);
+
+            foreach (var directory in directories)
+            {
+                foreach (var tag in directory.Tags)
+                {
+                    Console.WriteLine($"[{directory.Name}] {tag.Name} = {tag.Description}");
+
+                    // Find the EXIF data for the image
+                    if (tag.Name.Equals("Date/Time Original"))
+                    {
+                        Console.WriteLine($"[{directory.Name}] {tag.Name} = {tag.Description}");
+
+                        // Parse the EXIF data for the created date into a date we can use
+                        var fileDateTime = DateTime.ParseExact(tag.Description, "yyyy:MM:dd HH:mm:ss", new CultureInfo("en-GB"));
+                        
+                        // Create the destination folder
+                        var destinationDirectory = @"" + destinationPath + fileDateTime.Year + "-" + fileDateTime.Month + "-" + fileDateTime.Day;
+                        Console.WriteLine(destinationDirectory);
+
+                        // Check the destination directory exists
+                        if (!System.IO.Directory.Exists(destinationDirectory))
+                        {
+                            System.IO.Directory.CreateDirectory(destinationDirectory);
+                        }
+
+                        // Knowing the directory exists, we can now move the file
+                        if (File.Exists(file))
+                        {
+                            var destinationFile = destinationDirectory + "\\" + Path.GetFileName(file);
+
+                            if (File.Exists(destinationFile))
+                            {
+                                Random random = new Random();
+                                var randomNumber = random.Next(100);
+                                destinationFile = "" + Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + randomNumber + Path.GetExtension(file);
+                            }
+
+                            File.Move(file, destinationFile);
+                        }
+                    }
+
+                    // If the EXIF data tag wasn't found, find a new one (usually used for movie files)
+                    if (tag.Name.Equals("Created"))
+                    {
+                        Console.WriteLine($"[{directory.Name}] {tag.Name} = {tag.Description}");
+                        //Fri Apr 01 04:25:42 2011
+
+                        // Parse the EXIF data for the created date into a date we can use
+                        var fileDateTime = DateTime.ParseExact(tag.Description, "ddd MMM dd HH:mm:ss yyyy", new CultureInfo("en-GB"));
+
+                        // Create the destination folder
+                        var destinationDirectory = @"D:\\Photos\\Sorted\\" + fileDateTime.Year + "-" + fileDateTime.Month + "-" + fileDateTime.Day;
+                        Console.WriteLine(destinationDirectory);
+
+                        // Check the destination directory exists
+                        if (!System.IO.Directory.Exists(destinationDirectory))
+                        {
+                            System.IO.Directory.CreateDirectory(destinationDirectory);
+                        }
+
+                        // Knowing the directory exists, we can now move the file
+                        if (File.Exists(file))
+                        {
+                            File.Move(file, destinationDirectory + "\\" + Path.GetFileName(file));
+                        }
+                    }
+                }
+
+                if (directory.HasError)
+                {
+                    foreach (var error in directory.Errors)
+                        Console.WriteLine($"ERROR: {error}");
+                }
+            }
+        }
+
+        private void SortExistingPhotosIntoCorrectFolder()
+        {
+            var path = @"G:\\Photos\\";
+
+            if (File.Exists(path))
+            {
+                // This path is a file
+                ProcessFile(path);
+            }
+            else if (System.IO.Directory.Exists(path))
+            {
+                // This path is a directory
+                ProcessDirectory(path);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid file or directory.", path);
+            }
+        }
+
+        // Process all files in the directory passed in, recurse on any directories 
+        // that are found, and process the files they contain.
+        public void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = System.IO.Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = System.IO.Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        // Insert logic for processing found files here.
+        public void ProcessFile(string path)
+        {
+            Console.WriteLine("Processed file '{0}'.", path);
+            SortFileIntoFolder(path, @"G:\\Photos\\Sorted\\");
         }
     }
 }
